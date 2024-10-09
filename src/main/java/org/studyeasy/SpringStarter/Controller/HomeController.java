430ff038de4440cf6dbb640669039ef1d8ac1720
satyavenkat2011 <satyavenkat93881@gmail.com>
2024-10-09 13:45:51 +0530

    Initial commit

---
+++ src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
@@ -0,0 +1,35 @@
+package org.studyeasy.SpringStarter.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+    @GetMapping("/book")
+    public String book(Model model){
+        return "book";
+    }
+    @GetMapping("/buy")
+    public String buy(Model model){
+        return "buy";
+    }
+    @GetMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+    @GetMapping("/smarthome")
+    public String smarthome(Model model){
+        return "smarthome";
+    }
+    
+    }
+    
+   
+
