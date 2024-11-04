deb91b0843021822c1fd42e3297dcfebe02c7f47
satyavenkat2011 <147849939+satyavenkat2011@users.noreply.github.com>
2024-11-04 16:39:17 +0530

    Update HomeController.java

--- src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
+++ src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(Model model){
-        return "home";
+        return "index";
     }
     @GetMapping("/book")
     public String book(Model model){
